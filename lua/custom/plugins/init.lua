cb7658d4ff1ed1eeb55bf96242e57500854d7247
juliancit0 <juulianconti@gmail.com>
2026-08-20 18:52:04 -0400

    add diffview.nvim for side-by-side change review

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,21 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  { -- Side-by-side review of any git revision, with LSP live in both panes
+    'sindrets/diffview.nvim',
+    cmd = { 'DiffviewOpen', 'DiffviewClose', 'DiffviewFileHistory', 'DiffviewToggleFiles' },
+    keys = {
+      { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = '[G]it [D]iff working tree' },
+      { '<leader>gD', '<cmd>DiffviewOpen origin/HEAD...HEAD<cr>', desc = '[G]it [D]iff vs origin' },
+      { '<leader>gh', '<cmd>DiffviewFileHistory %<cr>', desc = '[G]it [H]istory of this file' },
+      { '<leader>gq', '<cmd>DiffviewClose<cr>', desc = '[G]it [Q]uit diff view' },
+    },
+    opts = {
+      enhanced_diff_hl = true,
+      view = {
+        merge_tool = { layout = 'diff3_mixed' },
+      },
+    },
+  },
+}
