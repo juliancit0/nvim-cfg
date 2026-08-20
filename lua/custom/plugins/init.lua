e526bba8cb3e88e6a79ca26c7b7685f19eba764a
juliancit0 <juulianconti@gmail.com>
2026-08-20 19:56:22 -0400

    add lazygit-style scroll keys to the diffview file panel

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -12,11 +12,30 @@ return {
       { '<leader>gh', '<cmd>DiffviewFileHistory %<cr>', desc = '[G]it [H]istory of this file' },
       { '<leader>gq', '<cmd>DiffviewClose<cr>', desc = '[G]it [Q]uit diff view' },
     },
-    opts = {
-      enhanced_diff_hl = true,
-      view = {
-        merge_tool = { layout = 'diff3_mixed' },
-      },
-    },
+    opts = function()
+      local actions = require 'diffview.actions'
+
+      -- Scroll the diff windows without leaving the file tree, using the same
+      -- keys lazygit uses for its main panel: J/K, <c-d>/<c-u>, page keys.
+      local scroll_from_panel = {
+        { 'n', 'J', actions.scroll_view(0.25), { desc = 'Scroll the diff down' } },
+        { 'n', 'K', actions.scroll_view(-0.25), { desc = 'Scroll the diff up' } },
+        { 'n', '<c-d>', actions.scroll_view(0.5), { desc = 'Scroll the diff down half a screen' } },
+        { 'n', '<c-u>', actions.scroll_view(-0.5), { desc = 'Scroll the diff up half a screen' } },
+        { 'n', '<pagedown>', actions.scroll_view(0.5), { desc = 'Scroll the diff down half a screen' } },
+        { 'n', '<pageup>', actions.scroll_view(-0.5), { desc = 'Scroll the diff up half a screen' } },
+      }
+
+      return {
+        enhanced_diff_hl = true,
+        view = {
+          merge_tool = { layout = 'diff3_mixed' },
+        },
+        keymaps = {
+          file_panel = scroll_from_panel,
+          file_history_panel = scroll_from_panel,
+        },
+      }
+    end,
   },
 }
